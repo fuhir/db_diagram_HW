db63bdd1da91f467259cde7b7e5327f2c0d2c9fb
Vitaly <fuhir@yandex.ru>
2023-05-24 10:47:06 +0300

    second HW

--- employees.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS Сотрудники (
-  сотрудник_id SERIAL PRIMARY KEY,
-  имя VARCHAR(80) UNIQUE NOT NULL,
-  отдел VARCHAR(80) NOT NULL,
-  начальник INTEGER REFERENCES Сотрудники
-);
